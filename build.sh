6b289090237935e87311e7ea1a782bb3c27ff797
Martin Aigner <martin@maigner.net>
2015-07-20 11:20:51 +0200

    added old songs from previous version

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 DIR=`pwd`
-export TEXINPUTS=$DIR/songs:$TEXINPUTS
+export TEXINPUTS=$DIR/songs:$DIR/latex-songs:$TEXINPUTS
 
 #TODO(martin): only perform multiple compilation steps when necessary
 cd books
 pdflatex songbook.tex
-songidx songbookidx.sxd songbookidx.sbx
+../latex-songs/songidx/songidx songbookidx.sxd songbookidx.sbx
 pdflatex songbook.tex
 
 cd $DIR
